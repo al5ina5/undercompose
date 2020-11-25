2227e0832c658e72fe951ed40b23fbbbba9c8c7e
tejasbaid <tejasbaid3@gmail.com>
2020-11-25 11:08:28 +0530

    Initial Commit

---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+export default function IndexPage() {
+    return (
+        <>
+            <div className='p-6 text-gray-800'>
+                This is the index page of a basic boilerplate built with NextJS + TailwindCSS!
+            </div>
+        </>
+    )
+}
